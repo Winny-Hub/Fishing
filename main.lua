73c2120ada916a6818754f1bca22e1cfe299d8a0
Winny-Hub <122193914+Winny-Hub@users.noreply.github.com>
2023-03-24 23:27:09 +0700

    Update and rename README.md to main.lua

---
+++ main.lua
@@ -0,0 +1,74 @@
+local RunService = game:GetService("RunService")
+local player = game.Players.LocalPlayer
+local hum = player.Character.HumanoidRootPart
+local mouse = player:GetMouse() 
+local tpservice= game:GetService("TeleportService")
+local VirtualUser= game:service'VirtualUser'
+
+local ToolsCache = game:GetService("ReplicatedStorage").ToolsCache[player.UserId]
+local plrTools
+local bLocation
+local fuckMonster
+local fuckMobby
+
+function teleport(loc)
+    bLocation = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    if game.Players.LocalPlayer.Character.Humanoid.Sit then
+        game.Players.LocalPlayer.Character.Humanoid.Sit = false
+    end
+    wait()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = loc
+end
+
+-- Get Tools Name (Just hidden the tool in replicatedstorage, savageeeee but characte win)
+for i, getTools in pairs(player.Character:GetChildren()) do
+    if getTools:IsA("Tool") and  getTools:FindFirstChild("GripC1") then
+        plrTools = getTools.Name
+    end
+end
+
+function EquipTool()
+    game:GetService("ReplicatedStorage").CloudFrameShared.DataStreams.SetEquippedItem:InvokeServer(2)
+    local args = {
+     [1] = game:GetService("ReplicatedStorage").ToolsCache:FindFirstChild(player.UserId)[plrTools]
+    }
+    game:GetService("ReplicatedStorage").CloudFrameShared.DataStreams.EquipTool:FireServer(unpack(args))
+end
+
+     if shared.toggle then
+    fuckMonster = RunService.Stepped:Connect(function()
+     for i, v in pairs(game.Workspace:GetChildren()) do
+     if v:FindFirstChild("Health") and v:FindFirstChild("IsSeaMonster") then
+        if game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
+                 
+                
+                    for i, getTools in pairs(player.Character:GetChildren()) do
+                        if getTools:IsA("Tool") and  getTools:FindFirstChild("GripC1") then
+                            plrTools = getTools.Name
+                        end
+                    end
+                 
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0,200,0)
+                    wait()
+                    game:GetService("ReplicatedStorage").CloudFrameShared.DataStreams.MonsterHit:FireServer(workspace[v.Name], tostring(plrTools), true)
+                    break
+                elseif not game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool") then
+                    EquipTool()
+               break
+            end
+        end
+     end
+     end)
+    else
+         fuckMonster:Disconnect()
+          teleport(CFrame.new(1.8703980445862, 53.57190322876, -188.37982177734))
+        end
+
+while toggle do wait()
+game:GetService("ReplicatedStorage").CloudFrameShared.DataStreams.processGameItemSold:InvokeServer("SellEverything")
+end
+
+repeat wait() until game:IsLoaded() 
+    game:GetService("Players").LocalPlayer.Idled:connect(function()
+    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
